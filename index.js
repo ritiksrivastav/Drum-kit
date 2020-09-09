629d326574ff140dc124f978cb9bd656c34e5ee2
ritiksrivastav <47292950+ritiksrivastav@users.noreply.github.com>
2020-09-09 19:14:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,31 @@ for(var i=0;i<document.querySelectorAll(".drum").length;++i){
       buttonAnimation(drumClick);
       switch (drumClick) {
           case "w":
-              var sTom1 = new Audio('sounds/tom-1.mp3');
+              var sTom1 = new Audio('tom-1.mp3');
               sTom1.play();              
               break;
           case "a":
-              var sTom2 = new Audio('sounds/tom-2.mp3');
+              var sTom2 = new Audio('tom-2.mp3');
               sTom2.play();              
               break;
           case "s":
-              var sTom3 = new Audio('sounds/tom-3.mp3');
+              var sTom3 = new Audio('tom-3.mp3');
               sTom3.play();              
               break;
           case "d":
-              var sTom4 = new Audio('sounds/tom-4.mp3');
+              var sTom4 = new Audio('tom-4.mp3');
               sTom4.play();              
               break;            
           case "j":
-              var sSnare = new Audio('sounds/snare.mp3');
+              var sSnare = new Audio('snare.mp3');
               sSnare.play();              
               break;
           case "k":
-              var sKick = new Audio('sounds/kick-bass.mp3');
+              var sKick = new Audio('kick-bass.mp3');
               sKick.play();              
               break;
           case "l":
-              var sCrash = new Audio('sounds/crash.mp3');
+              var sCrash = new Audio('crash.mp3');
               sCrash.play();              
               break;
           default:
@@ -46,31 +46,31 @@ document.addEventListener("keydown",function (event) {
 function makeSound(key){
     switch (key) {
         case "w":
-            var sTom1 = new Audio('sounds/tom-1.mp3');
+            var sTom1 = new Audio('tom-1.mp3');
             sTom1.play();              
             break;
         case "a":
-            var sTom2 = new Audio('sounds/tom-2.mp3');
+            var sTom2 = new Audio('tom-2.mp3');
             sTom2.play();              
             break;
         case "s":
-            var sTom3 = new Audio('sounds/tom-3.mp3');
+            var sTom3 = new Audio('tom-3.mp3');
             sTom3.play();              
             break;
         case "d":
-            var sTom4 = new Audio('sounds/tom-4.mp3');
+            var sTom4 = new Audio('tom-4.mp3');
             sTom4.play();              
             break;            
         case "j":
-            var sSnare = new Audio('sounds/snare.mp3');
+            var sSnare = new Audio('snare.mp3');
             sSnare.play();              
             break;
         case "k":
-            var sKick = new Audio('sounds/kick-bass.mp3');
+            var sKick = new Audio('kick-bass.mp3');
             sKick.play();              
             break;
         case "l":
-            var sCrash = new Audio('sounds/crash.mp3');
+            var sCrash = new Audio('crash.mp3');
             sCrash.play();              
             break;
         default:
